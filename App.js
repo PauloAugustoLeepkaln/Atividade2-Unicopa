21623e93b3d21cee3c49d56c948a152a43cf09b8
PauloAugustoLeepkaln <paulo70306@edu.unipar.br>
2026-05-05 20:12:02 -0300

    Feita a terceira etapa

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import {
   StyleSheet,
   Text,
-  View,
   Image,
   ImageBackground,
   FlatList,
 } from "react-native";
-import GameCard from "./app/components/GameCard";
-import copaData from "./app/assets/data/copaData.json";
 import { useState } from "react";
-import { SectionList } from "react-native";
+import copaData from "./app/assets/data/copaData.json";
+import DiaCard from "./app/components/DiaCard";
 
 export default function App() {
   const [jogos, setJogos] = useState(copaData.jogos);
-  const [dadosCopa, setDadosCopa] = useState(copaData);
+
   const agruparPorData = (jogos) => {
     return jogos.reduce((acc, jogo) => {
       const data = jogo.data_brasilia;
@@ -24,6 +22,7 @@ export default function App() {
       return acc;
     }, {});
   };
+
   const jogosAgrupados = agruparPorData(jogos);
   const jogosTratados = Object.keys(jogosAgrupados).map((data) => {
     return {
@@ -36,23 +35,19 @@ export default function App() {
     <ImageBackground
       style={styles.container}
       source={require("./app/assets/bg-overlay.png")}
+      resizeMode="cover"
     >
       <Image style={styles.logo} source={require("./app/assets/unicopa.png")} />
 
       <Text style={styles.title}>CALENDÁRIO</Text>
-      <SectionList
-        sections={jogosTratados}
-        keyExtractor={(item, index) => item.id || index.toString()}
-        renderItem={() => null}
-        renderSectionHeader={({ section }) => (
-          <View style={styles.card}>
-            <Text style={styles.data}>
-              {section.title.split("-").slice(1).reverse().join("/")}
-            </Text>
-            {section.data.map((jogo) => (
-              <GameCard key={jogo.id} game={jogo} />
-            ))}
-          </View>
+
+      <FlatList
+        data={jogosTratados}
+        keyExtractor={(item) => item.title}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 30 }}
+        renderItem={({ item }) => (
+          <DiaCard data={item.title} jogos={item.data} />
         )}
       />
     </ImageBackground>
@@ -61,13 +56,13 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    height: "100%",
     width: "100%",
+    height: "100%",
     backgroundColor: "#040b13",
     alignItems: "center",
   },
   logo: {
-    marginTop: 20,
+    marginTop: 40,
     width: 200,
     height: 50,
     resizeMode: "contain",
@@ -77,67 +72,5 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: "700",
     color: "white",
-  },
-  card: {
-    marginTop: 20,
-    backgroundColor: "#0c1b2a",
-    width: 320,
-    borderRadius: 12,
-    padding: 15,
-  },
-  data: {
-    color: "#f2cc2f",
-    fontSize: 22,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-
-  jogo: {
-    marginBottom: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: "#1e2d3d",
-    paddingBottom: 15,
-  },
-  grupo: {
-    color: "#8fa3b8",
-    fontSize: 12,
-    marginBottom: 10,
-  },
-  linhaPrincipal: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  time: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  bandeira: {
-    width: 28,
-    height: 28,
-    borderRadius: 14,
-  },
-  sigla: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 16,
-  },
-  horario: {
-    alignItems: "center",
-  },
-  hora: {
-    color: "white",
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  local: {
-    marginTop: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  subTitulo: {
-    color: "#8fa3b8",
-    fontSize: 12,
   },
 });
