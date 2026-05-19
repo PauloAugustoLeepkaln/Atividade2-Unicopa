678eae6edf8d7491d6e7df7594227141ca03f87e
PauloAugustoLeepkaln <paulo70306@edu.unipar.br>
2026-05-18 21:30:28 -0300

    Etapa 10 arrumada

--- app/utils/supabase.js
+++ app/utils/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Colocando as credenciais diretamente (em formato de texto com aspas)
 const supabaseUrl = "https://lpkmrajvwzdjydkyyqzb.supabase.co";
-const supabaseKey = "Sua_Chave_Gigante_Aqui";
+const supabaseKey = "sb_publishable_eJD5CGmw4vvvIBOrq2K2GA_k16eeeyL";
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
